def2feaa4d381784fcf30e92d9f87a960f232baf
Alexei-Kamaev <alexei-kamaev@yandex.ru>
2023-09-08 14:01:47 +0300

    finish

---
+++ tasksHard_1.js
@@ -0,0 +1,14 @@
+'use strict';
+{
+    const summa = +prompt('Введите сууму дохода');
+    if (isNaN(summa)) {
+        console.log('Введите число');
+        }
+        else if (summa <= 15000) {
+        console.log(`С доходом ${summa}р, сумма налога составит: ${(summa * .13).toFixed(2)} рублей`);
+        } else if (summa > 50000) {
+        console.log(`С доходом ${summa}р, сумма налога составит: ${(summa * .30).toFixed(2)} рублей`);
+    } else {
+        console.log(`С доходом ${summa}р, сумма налога составит: ${(summa * .20).toFixed(2)} рублей`);
+    }
+}
